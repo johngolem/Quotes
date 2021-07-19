80ce023cf30d7612b477934c536c457724973ab6
john paul <jpaulopiy@gmail.com>
2021-07-19 14:19:33 +0300

    Auto-generated commit [ci skip]

--- src/app/downvote.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DownvotePipe } from './downvote.pipe';
-
-describe('DownvotePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DownvotePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/upvote.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UpvotePipe } from './upvote.pipe';
-
-describe('UpvotePipe', () => {
-  it('create an instance', () => {
-    const pipe = new UpvotePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
